04b40093b3f7f2a39962f9e10c1c69396901189f
Caique Pereira Alves <caiquewin@gmail.com>
2021-04-19 19:36:32 -0300

    Primeiro dia NLW

---
+++ src/Server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const app = express();
+
+/**
+* GET = Buscas
+* POST = Criação
+* DELETE = Deletar
+* PATH = Alterar uma informação especifica
+*/
+
+app.get("/",(request,response)=>{
+    return response.json({
+        message:"Ola NLW 05 🤞"
+    })
+})
+
+//ROTA DE POST
+app.post("/",(request,response) =>{
+    return response.json({
+        message:"Usuário salvo com sucesso ✔"
+    })
+})
+
+app.listen(3333,()=>console.log("Está funcionando 🧙‍♂️"));
